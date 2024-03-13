e7a78645a7b984fdcb6ec7fd38cd29b18e4526bf
Dzimitry1 <dimamail87@gmail.com>
2024-03-13 19:30:05 +0300

    1

---
+++ lesson03/app.js
@@ -0,0 +1,21 @@
+ // Number
+ // bigint
+ // String
+ // Boolean
+ // Undefined
+ // Null
+ // Symbol
+ // Object
+
+const titleProject = 'Оценка стоимости';
+console.log (typeof titleProject);
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+console.log (typeof screensValue);
+const screenPrice = 8;
+console.log (typeof screenPrice);
+const percentage = 20;
+console.log (typeof percentage);
+const fullPrice = 20345;
+console.log (typeof fullPrice);
+const responsive = true;
+console.log (typeof responsive);
